107dc7b7327ebe3cf624d93d5b6d37eec52ff919
Rozaki0318 <r.ozaki0318@gmail.com>
2023-07-16 07:26:37 +0900

    add error handling to read.py

--- crud/read.py
+++ crud/read.py
@@ -1,24 +1,33 @@
 import json
 import boto3
+from botocore.exceptions import ClientError
 import logging
+
+logger = logging.getLogger(__name__)
 
 def lambda_handler(event, context):
     print("READ OF CRUD FUNCTION")
-    print(event)
+    #print(event)
     print("HTTP METHOD: {}".format(event['httpMethod']))
-
+    
     dynamodb_client = boto3.client('dynamodb', endpoint_url='http://dynamodb-local:8000')
-
-    print("##DYNAMDB CLIENT##:{}".format(dynamodb_client))
-
+    
     res = dynamodb_client.list_tables()
-    print("##DYNAMODB TABLE##:{}".format(res['TableNames']))
+        
+    print("##DYNAMODB TABLE##:{}".format(res['TableNames']))        
     table = res['TableNames'][0]
 
-    res_scan = dynamodb_client.scan(
-        TableName = table,
-    )
+    try:
+        res_scan = dynamodb_client.scan(
+            TableName = table,
+        )
+    except ClientError as err:
+        logger.error(
+            "Couldn't scan. Here's why: %s: %s",
+                err.response['Error']['Code'], err.response['Error']['Message'])
+        raise
 
+    
     return {
         "statusCode": 200,
         "headers":{
@@ -32,4 +41,3 @@ def lambda_handler(event, context):
             }]
         ),
     }
-
