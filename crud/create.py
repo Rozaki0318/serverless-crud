7f795a313a4c1f3344761887927ab51a69e7b5da
Rozaki0318 <r.ozaki0318@gmail.com>
2023-07-16 09:30:53 +0900

    add error handling

--- crud/create.py
+++ crud/create.py
@@ -1,14 +1,17 @@
 import json
 import boto3
+from botocore.exceptions import ClientError
 import logging
+
+logger = logging.getLogger(__name__)
 
 def lambda_handler(event, context):
     print("CREATE OF CRUD FUNCTION")
-    print(event)
+    #print(event)
     print("HTTP METHOD: {}".format(event['httpMethod']))
 
-
     dynamodb_client = boto3.client('dynamodb', endpoint_url='http://dynamodb-local:8000')
+    
     table_names = dynamodb_client.list_tables()
     table = table_names['TableNames'][0]
 
@@ -17,14 +20,22 @@ def lambda_handler(event, context):
     print(data['id'])
     print(data['name'])
 
-    res = dynamodb_client.put_item(
-        TableName=table,
-        Item={
-            "id": {"S": data['id']},
-            "name": {"S": data['name']}
-        }
-    )
-    print(res)
+    try:
+        res = dynamodb_client.put_item(
+            TableName=table,
+            Item={
+                "id": {"S": data['id']},
+                "name": {"S": data['name']}
+                },
+            Expected={
+                "id": {"Exists": False}
+                }
+            )
+    except ClientError as err:
+        logger.error(
+            "Couldn't insert data.Here's why: %s: %s",
+                err.response['Error']['Code'], err.response['Error']['Message'])
+        raise
 
     return {
         "statusCode": 200,
@@ -33,7 +44,7 @@ def lambda_handler(event, context):
         },
         "body": json.dumps(
             [{
-                "Data": 'hello post',
+                "Data": res,
             }]
         ),
     }
--- crud/delete.py
+++ crud/delete.py
@@ -1,14 +1,18 @@
 import json
 import boto3
+from botocore.exceptions import ClientError
 import logging
+
+logger = logging.getLogger(__name__)
 
 def lambda_handler(event, context):
     print("DELETE OF CRUD FUNCTION")
-    print(event)
+    #print(event)
     print("HTTP METHOD: {}".format(event['httpMethod']))
 
 
     dynamodb_client = boto3.client('dynamodb', endpoint_url='http://dynamodb-local:8000')
+    
     table_names = dynamodb_client.list_tables()
     table = table_names['TableNames'][0]
 
@@ -17,14 +21,19 @@ def lambda_handler(event, context):
     print(data['id'])
     #print(data['name'])
 
-    res = dynamodb_client.delete_item(
-        TableName=table,
-        Key={
-            "id": {"S": data['id']},
-            #"name": {"S": data['name']}
-        }
-    )
-    print(res)
+    try:
+        res = dynamodb_client.delete_item(
+            TableName=table,
+            Key={
+                "id": {"S": data['id']},
+                #"name": {"S": data['name']}
+                }
+            )
+    except ClientError as err:
+        logger.error(
+            "Couldn't insert data.Here's why: %s: %s",
+                err.response['Error']['Code'], err.response['Error']['Message'])
+        raise
 
     return {
         "statusCode": 200,
@@ -35,7 +44,7 @@ def lambda_handler(event, context):
         },
         "body": json.dumps(
             [{
-                "Data": 'hello post',
+                "Data": res,
             }]
         ),
     }
